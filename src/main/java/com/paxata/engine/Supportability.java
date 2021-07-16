06db6db76ed7a73ff4c3539092bedbbcf3f87ebc
Sutanu Paul <sutanu.paul@datarobot.com>
2021-07-16 15:17:36 +0530

    last commit

--- src/main/java/com/paxata/engine/Supportability.java
+++ src/main/java/com/paxata/engine/Supportability.java
@@ -1,9 +1,7 @@
 package com.paxata.engine;
 
 
-import io.fabric8.kubernetes.api.model.Container;
-import io.fabric8.kubernetes.api.model.Pod;
-import io.fabric8.kubernetes.api.model.PodList;
+import io.fabric8.kubernetes.api.model.*;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.LogWatch;
@@ -11,13 +9,14 @@ import io.fabric8.kubernetes.client.dsl.PrettyLoggable;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Supportability {
     public static final String MONGO_NAMESPACE = "mongo";
-    public static final String MY_NAMESPACE = "sutanu-supportability";
+    public static final String MY_NAMESPACE = "sutanu-2669";
     public static final String PAX_POD_NAME = "pax-installation-paxserver";
     public static final String PAX_CONTAINER = "paxserver";
     public static final String MONGO_POD_NAME = "mongo-deployment-0";
@@ -61,6 +60,7 @@ public class Supportability {
                 .inContainer(container)
                 .file(filePathInContainer)
                 .copy(downloadLocation.toPath());
+        client.namespaces();
     }
 
     public void readLog(String namespace, String podname, String container, int lineCount, File filePath){//https://www.programcreek.com/java-api-examples/index.php?api=io.fabric8.kubernetes.client.dsl.LogWatch
@@ -86,10 +86,13 @@ public class Supportability {
             String log = client.pods().inNamespace(namespace)
                     .withName(podname)
                     .inContainer(container)
+                    .sinceSeconds(1800)
                     .getLog();
             createFolder(filePath);
             if (!log.isEmpty()) {
                 FileWriter fileWriter = new FileWriter(new File(filePath.getAbsolutePath()));
+                String []logLines=log.split("\n");
+                String filteredLog=Arrays.stream(logLines).filter(p -> p.contains("listener")).reduce("",(logLine,cumulativLog) -> cumulativLog+"\n"+logLine);
                 fileWriter.write(log);
                 fileWriter.close();
             }
@@ -159,10 +162,101 @@ public class Supportability {
 
 
     public static void main(String[] args) {
-        Supportability supportability = new Supportability();
-        supportability.getMongoData();
-        supportability.getCoreData();
-        supportability.getPipelineProxyData();
-        supportability.getPipelineData();
+//        Supportability supportability = new Supportability();
+//        supportability.getMongoData();
+//        supportability.getCoreData();
+//        supportability.getPipelineProxyData();
+//        supportability.getPipelineData();
+//        Pod pod=new PodBuilder()
+//                .withNewMetadata()
+//                    .withName("task-pv-pod")
+//                    .withNamespace(MY_NAMESPACE)
+//                .endMetadata()
+//                .withNewSpec()
+//                    .withVolumes(
+//                        new VolumeBuilder()
+//                                .withName("authkey-vol-name")
+//                                .withNewPersistentVolumeClaim()
+//                                .withClaimName("authkey-pvc")
+//                                .endPersistentVolumeClaim()
+//                                .build())
+//                    .addNewContainer()
+//                        .withName("task-pv-container")
+//                        .withImage("nginx")
+//                        .addNewPort()
+//                            .withContainerPort(80)
+//                            .withName("http-server")
+//                        .endPort()
+//                        .withVolumeMounts(
+//                            new VolumeMountBuilder()
+//                                .withMountPath("/usrtest2")
+//                                .withName("authkey-vol-name")
+//                                .build()
+//                )                 .endContainer()
+//                .endSpec()
+//                .build();
+//
+
+String podname="task-pv-pod14";
+        Pod pod1 = new PodBuilder()
+                .withNewMetadata()
+                .withName(podname)
+                .addToLabels("paxata.com/connector-artifact-id", "connector-big-query")
+                .addToLabels("app.kubernetes.io/name", "connector")
+                .addToLabels("app.kubernetes.io/instance", "cebc167d-14ab-4799-b4ee-cfc834399cc2")
+                .addToLabels("paxata.com/installation-name", "pax-installation")
+                .withNamespace("sutanu-2669")
+                .addNewOwnerReference()
+                .withUid("2b962a7c-92a6-41e8-9b8d-b772e48661e6")
+                .withName("pax-installation-paxserver")
+                .withApiVersion("apps/v1")
+                .withController(true)
+                .withKind("Pod")
+                .endOwnerReference()
+                .endMetadata()
+                .withNewSpec()
+                .addNewImagePullSecret()
+                .withName("pax-installation-dtr")
+                .endImagePullSecret()
+                .addNewContainer()
+                .withName(podname)
+                .withImage("011447054295.dkr.ecr.us-west-2.amazonaws.com/paxata/runnable_pr_connector_big-query:2021.2.pr.2021.2_DATAPREP-2669")
+                .withImagePullPolicy("Always")
+                .addNewEnv()
+                .withName("ADDITIONAL_JVM_ARGS")
+                .withValue("-XX:+AggressiveOpts -XX:-UseCompressedOops -XX:+HeapDumpOnOutOfMemoryError -Dpx.idle.timeout.minutes=60")
+                .endEnv()
+                .addNewPort().withContainerPort(8110).endPort()
+                .withNewReadinessProbe()
+                .withNewHttpGet()
+                .withPath("/health")
+                .withPort(new IntOrString(8110))
+                .endHttpGet()
+                .withInitialDelaySeconds(2)
+                .withPeriodSeconds(1)
+                .endReadinessProbe()
+                .withNewResources()
+                .addToRequests("cpu", new Quantity("250m"))
+                .addToRequests("memory",  new Quantity("1024m"))
+                .endResources()
+                .withVolumeMounts(
+                        new VolumeMountBuilder()
+                                .withMountPath("/usr/local/paxata/connector/tmp/authkeystore")
+                                .withName("authkey-vol-name")
+                                .build()
+                )
+                .endContainer()
+                .withRestartPolicy("Never")
+                .withVolumes(
+                        new VolumeBuilder()
+                                .withName("authkey-vol-name")
+                                .withNewPersistentVolumeClaim()
+                                .withClaimName("pax-installation-pvc")
+                                .endPersistentVolumeClaim()
+                                .build())
+                .endSpec()
+                .build();
+        new Supportability().client.pods().inNamespace(MY_NAMESPACE).create(pod1);
+        System.out.println(pod1.getMetadata().getName());
     }
 }
